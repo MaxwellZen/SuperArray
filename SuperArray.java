9ac475d295942457f09848d6ab76404eb7f767ea
MaxwellZen <maxwell.zen@gmail.com>
2020-10-31 17:49:30 -0400

    drafted the methods

---
+++ SuperArray.java
@@ -0,0 +1,31 @@
+public class SuperArray {
+  private String[] data;
+  private int size;
+  public SuperArray() {
+    data = new String[10];
+    size = 0;
+  }
+  public int size() {
+    return size;
+  }
+  public boolean add(String element) {
+    if (size == data.length) resize();
+    data[size] = element;
+    size += 1;
+    return true;
+  }
+  public String get(int index) {
+    return data[index];
+  }
+  public String set(int index, String element) {
+    data[index] = element;
+    return element;
+  }
+  private void resize() {
+    String[] new = new String[data.length + 10];
+    for (int i = 0; i < size; i++) {
+      new[i] = data[i];
+    }
+    data = new;
+  }
+}
