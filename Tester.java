0f0d241684fe6167d12b433453cd73a832c800aa
MaxwellZen <maxwell.zen@gmail.com>
2020-10-31 18:14:57 -0400

    tested my methods

---
+++ Tester.java
@@ -0,0 +1,19 @@
+public class Tester {
+  public static void main (String[] args) {
+    SuperArray words = new SuperArray();
+    String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    for (int i = 0; i < alphabet.length(); i++) {
+      words.add(alphabet.substring(i, i+1));
+    }
+    for (int i = 0; i < words.size(); i++) {
+      System.out.println(words.get(i));
+    }
+    for (int i = 0; i < words.size() - 1; i++) {
+      words.set(i, words.get(i+1));
+    }
+    words.set(words.size()-1, "a");
+    for (int i = 0; i < words.size(); i++) {
+      System.out.println(words.get(i));
+    }
+  }
+}
