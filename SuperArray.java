c701a7fc9ffb5d546bfc3068fe8b9454c4e03ca3
MaxwellZen <maxwell.zen@gmail.com>
2020-11-05 00:14:55 -0500

    wrote and briefly tested the clear, add, remove, indexOf, and toArray methods

--- SuperArray.java
+++ SuperArray.java
@@ -5,6 +5,10 @@ public class SuperArray {
     data = new String[10];
     size = 0;
   }
+  public SuperArray(int InitialCapacity) {
+    data = new String[InitialCapacity];
+    size = 0;
+  }
   public int size() {
     return size;
   }
@@ -23,7 +27,7 @@ public class SuperArray {
     return old;
   }
   private void resize() {
-    String[] ans = new String[data.length + 10];
+    String[] ans = new String[data.length*2];
     for (int i = 0; i < size; i++) {
       ans[i] = data[i];
     }
@@ -45,5 +49,39 @@ public class SuperArray {
       if (data[i].equals(s)) return true;
     }
     return false;
+  }
+  public void clear() {
+    data = new String[size];
+    size = 0;
+  }
+  public void add(int index, String element) {
+    if (size == data.length) resize();
+    for (int i = size - 1; i >= index; i--) {
+      data[i+1] = data[i];
+    }
+    data[index] = element;
+    size++;
+  }
+  public String remove(int index) {
+    String ans = data[index];
+    for (int i = index; i < size-1; i++) {
+      data[i] = data[i+1];
+    }
+    data[size-1] = null;
+    size--;
+    return ans;
+  }
+  public int indexOf(String s) {
+    for (int i = 0; i < size; i++) {
+      if (data[i].equals(s)) return i;
+    }
+    return -1;
+  }
+  public String[] toArray() {
+    String[] ans = new String[size];
+    for (int i = 0; i < size; i++) {
+      ans[i] = data[i];
+    }
+    return ans;
   }
 }
--- Tester.java
+++ Tester.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Tester {
   public static void main (String[] args) {
     SuperArray words = new SuperArray();
@@ -26,5 +27,12 @@ public class Tester {
     System.out.println(words.contains("aa"));
     String b = null;
     System.out.println(words.contains(b));
+    words.add(3, "asdf");
+    System.out.println(words);
+    System.out.println(words.remove(0));
+    System.out.println(words);
+    System.out.println(words.indexOf("c"));
+    System.out.println(Arrays.toString(words.toArray()));
+
   }
 }
